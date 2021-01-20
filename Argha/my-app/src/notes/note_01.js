fc3fe3424a88dec11a61ee8b2ae7aa88c806baed
DigitalEsca9e <43222165+DigitalEsca9e@users.noreply.github.com>
2021-01-20 12:28:16 -0500

    editable notes
    open notes_01 for the running code

--- Argha/my-app/src/notes/note_01.js
+++ Argha/my-app/src/notes/note_01.js
@@ -26,7 +26,7 @@ class note_01 extends Component {
       var output= document.getElementById('notes');// gets the editable note area 
       var photo = document.createElement('img');// creates a new image element 
       photo.setAttribute("src", this.state.selectedFile);//assigns the src of the new image element to the src identified in the handleChange function  
-      output.appendChild(photo);//appends the new image elemet to the div as a child element 
+      output.appendChild(photo);//appends the new image elemet to the div as a child element
       this.setState({ //resets the state of the 'selectedFile' element of the contructor to null
         selectedFile: null
       })
@@ -38,8 +38,8 @@ class note_01 extends Component {
   render(){
       return (
               <div>
-                <h1 palceholder="Title" contentEditable="true"></h1>
-                <div id="notes" placeholder="Click here" contentEditable="true">
+                <h1 id="notes_title" placeholder="Title" contenteditable="true"></h1>
+                <div id="notes" placeholder="type something" contenteditable="true">
                 </div>
                 <input type="file" onChange={this.handleChange}/>
                 <input type="submit" onClick={this.filePost}/>
