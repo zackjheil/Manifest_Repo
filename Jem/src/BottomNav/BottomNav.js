515b8b7b08d87f660a438975af7154c9e462119f
Jem <61563594+jemgenn@users.noreply.github.com>
2021-01-31 15:48:38 -0500

    Added popover
    Popover is on the add button on bottom nav but also added comments on some CSS because I can't get some stuff to work

--- Jem/src/BottomNav/BottomNav.js
+++ Jem/src/BottomNav/BottomNav.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import './BottomNav.css';
+import AddSymbol from './add_symbol.svg';
+import {OverlayTrigger} from 'react-bootstrap';
+import Popover from '../Popover/Popover';
 
 class BottomNav extends Component
 {
@@ -9,6 +12,9 @@ class BottomNav extends Component
             <nav className="bottomNav">
                 <div className="bottomContainer">
                     <a  className="bottomLink" href="/notes/note_01">Type something...</a>
+                    <OverlayTrigger trigger="click" placement="top" overlay={Popover}>
+                        <img className="bottomAdd" src={AddSymbol}/>
+                    </OverlayTrigger>
                 </div>
             </nav>
         )
