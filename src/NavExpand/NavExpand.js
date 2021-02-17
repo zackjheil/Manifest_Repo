224a18fd04408825aac7dc87c70113870b93c0e6
DigitalEsca9e <43222165+DigitalEsca9e@users.noreply.github.com>
2021-02-17 10:00:05 -0500

    changes to navbar
    changed the main navigation from drop down to collapse-able sidebar

--- src/NavExpand/NavExpand.js
+++ src/NavExpand/NavExpand.js
@@ -2,24 +2,69 @@ import React, {Component} from 'react';
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 import './NavExpand.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from 'react-sidebar';
+import { Link } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa'
+
 
 class NavExpand extends Component
 {
+   constructor (props){
+        super(props);
+        this.state = {
+            showSidebar: false
+        }
+    this.showNav=this.showNav.bind(this);
+    }
+    showNav(){
+        if(this.state.showSidebar===false)
+        {
+            this.setState(
+                {
+                    showSidebar: true
+                }
+            )
+        }
+        else if(this.state.showSidebar===true){
+            this.setState(
+                {
+                    showSidebar: false
+                }
+            )
+        }
+    }
     render()
     {
         return(
+            <div>
             <Navbar class="navbar sticky-top" collapseOnSelect expand="menuButton" className="navExpand">
-                <Navbar.Brand className="navTitle" href="/">Manifest</Navbar.Brand>
-                <Navbar.Toggle/>
-                <Navbar.Collapse>
-                    <Nav className="mr-auto">
-                        <Nav.Link className="navLinks" href="/">Profile</Nav.Link>
-                        <Nav.Link className="navLinks" href="/">Main</Nav.Link>
-                        <Nav.Link className="navLinks" href="/">Emotions Log</Nav.Link>
-                        <Nav.Link className="navLinks" href="/">Settings</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                <Link to="#" className="menu-bars">
+                    <FaIcons.FaBars onClick={this.showNav}/>
+                </Link>
+                <Navbar.Brand className="navTitle" href="/">Manifest</Navbar.Brand>      
             </Navbar>
+            <nav className={this.state.showSidebar ? "sideNav active" : "sideNav"}>
+            <ul className="navigation">
+                <li className="navToggle">
+                    <Link to="#" className="menu-bars">
+                        <FaIcons.FaBars onClick={this.showNav}/>
+                    </Link>
+                </li>
+                <li className="navLinks">
+                    <Link to="/">Profile</Link>
+                </li>
+                <li className="navLinks">
+                    <Link to="/">Main</Link>
+                </li>
+                <li className="navLinks">
+                    <Link to="/">Emotions Log</Link>
+                </li>
+                <li className="navLinks">
+                    <Link to="/">Settings</Link>
+                </li>
+            </ul>
+            </nav>
+        </div>
         )
     }
 }
