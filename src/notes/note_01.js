689a97da8cc1be79bc1220c1ac0a6e2f94383310
JoshuaYDucharme-Baribeau <60009754+JoshuaYDucharme-Baribeau@users.noreply.github.com>
2021-03-03 10:45:20 -0500

    Recommitting Jem's work
    Recommitting Jem's work so Jogie can push

--- src/notes/note_01.js
+++ src/notes/note_01.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import '../App.css';
+import './note_01.css';
+import '../AddContent/AddBar.css';
+import {OverlayTrigger} from 'react-bootstrap';
+import Popover from '../Popover/Popover';
 import { Link } from 'react-router-dom';
-import AddBar from '../AddContent/AddBar';
+//import AddBar from '../AddContent/AddBar';
 import * as IoIcons from 'react-icons/io';
 import Localbase from 'localbase';
+import autosize from 'autosize';
 var counter=1;
 export class note_01 extends Component {
   
@@ -36,6 +41,7 @@ handleTxtChange(event){ // function to retrieve the test from the textarea in th
     txtNote: event.target.value
   })
 }
+
 /*displayTextEditor(){ // fumction to display the Text Editor Page
   this.setState({
     displayNotePage: false,
@@ -99,13 +105,28 @@ render(){
                 </Link>
                 <div id="notes_title" placeholder="Title" contentEditable="true"></div>
                 <br/>
-                <div id="notes" placeholder="type something"contentEditable="true">{/*onClick={this.displayTextEdito*/}
+                <div id="notes" placeholder="type something">{/*onClick={this.displayTextEdito*/}
                 </div>
-                <input type="file" onChange={this.handleChange}/>
+                {/*<input type="file" onChange={this.handleChange}/>
                 <input type="submit" onClick={this.filePost}/>
-                {/*<button  onClick={this.displayTextEditor}>Text Editor</button>*/}
-                <AddBar></AddBar>
+                {/*<button  onClick={this.displayTextEditor}>Text Editor</button>
+                {/*<button type="submit" onClick={textNote}>text Note</button>*/}
+                <script>
+                  autosize(document.querrySelectorAll('.LNote'));
+                </script>
                 <button type="submit" onClick={Added}>Do Something</button>
+                {/*<button type="submit" onClick={dummy}>Dummy</button>*/}
+                <nav className="addBar">
+                  <button type="submit" className="popOption" onClick={textNote}>Text</button>
+                  <OverlayTrigger trigger="click" placement="top" overlay={
+                  <input type="file" onChange={this.handleChange}/>
+                  }>
+                    <a className="popOption">Image</a>
+                  </OverlayTrigger>
+                  <input className="popOption" type="submit" onClick={this.filePost}/>
+                  <a href="/" className="popOption">Option</a>
+                  <a href="/" className="popOption">Option</a>
+                </nav>
               </div>
       )
     } 
@@ -136,6 +157,24 @@ let db = new Localbase('Stored')
 export function showAll(){
 
 }
+export function textNote(){
+  var output= document.getElementById('notes');
+  var tArea=document.createElement('textarea');
+  tArea.className="LNote";
+  //tArea.contentEditable=true;
+  //tArea.textContent="";
+  tArea.id=counter;
+  autosize(tArea);
+  counter+=1;
+  output.appendChild(tArea);
+}
+/*export function dummy(){
+  var output=document.getElementById('notes');
+  var cont=document.getElementById('1').value;
+  var input=document.createElement('textarea')
+  input.textContent=cont;
+  output.appendChild(input);
+}*/
 
 export function Added() {
   db.collection('n1').add({
@@ -153,12 +192,14 @@ for(var i=1; i<counter; i++){
     })
     
   }
-  //db.collection('n1').add({
-  //id:i,
-  //Name: 'ASDASDASD',
-  //Content: document.getElementById('notes'),
-  //content : 'Hi there'
-//})
+  else if(ele.getAttribute("className")==="LNote"){
+    db.collection('n1').add({
+      id:i,
+      class:ele.className,
+      type:'textNote',
+      content:ele.value
+    })
+  }
 }
 console.log('files to db');
 console.log(counter);
