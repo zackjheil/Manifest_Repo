80cd2163a83c466a47082893870eb99310403a16
JoshuaYDucharme-Baribeau <60009754+JoshuaYDucharme-Baribeau@users.noreply.github.com>
2021-04-13 18:36:14 -0400

    Updated icons and added some react installs
    Updated icons and updating react installs.

--- src/containers/NavExpand/NavExpand.js
+++ src/containers/NavExpand/NavExpand.js
@@ -6,6 +6,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import ManiLogo from "../../assets/Manifest_Logo.svg"
+import BoardsButton from "../../assets/Boards_Icon.svg"
+import SettingsButton from "../../assets/Settings_Icon.svg"
+import InfoButton from "../../assets/Info_Icon.svg"
 import PlusButton from "../../assets/Buttons/AddButton.svg"
 
 
@@ -42,7 +46,7 @@ class NavExpand extends Component {
                 <Link to="#" className="menu-bars">
                     <FaIcons.FaBars onClick={this.showNav}/>
                 </Link>
-                <Navbar.Brand className="navTitle" href="/"><h1 className="h1Title">Manifest <img src={PlusButton} alt="Add" style={{width:74, height:74, marginLeft: 0}}/></h1> </Navbar.Brand>      
+                <Navbar.Brand className="navTitle" href="/"><h1 className="h1Title">Manifest <img src={ManiLogo} alt="Add" style={{width:74, height:74, marginLeft: 0}}/></h1> </Navbar.Brand>      
             </Navbar>
             <nav className={this.state.showSidebar ? "sideNav active" : "sideNav"}>
                 <div className="navigation">
@@ -52,18 +56,18 @@ class NavExpand extends Component {
                         </Link>
                     </p>
 
-                    <p><center> <img src={PlusButton} alt="Add" style={{width:74, height:74, marginLeft: 0}}/> </center></p>
+                    <p><center> <img src={ManiLogo} alt="Add" style={{width:74, height:74, marginLeft: 0}}/> </center></p>
 
                     <p className="navLinks">
-                        <Link to="/"> <img src={PlusButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> Boards </Link>
+                        <Link to="/"> <img src={BoardsButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> Boards </Link>
                     </p>
 
                     <p className="navLinks">
-                        <Link to="/About"> <img src={PlusButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> About</Link>
+                        <Link to="/About"> <img src={InfoButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> About</Link>
                     </p>
             
                     <p className="navLinks">
-                        <Link to="/settings"> <img src={PlusButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> Settings</Link>
+                        <Link to="/settings"> <img src={SettingsButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> Settings</Link>
                     </p>
                 </div>
             </nav>
