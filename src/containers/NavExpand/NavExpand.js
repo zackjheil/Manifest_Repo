526135d17cd9c098d1315c707d87be4464810deb
JoshuaYDucharme-Baribeau <60009754+JoshuaYDucharme-Baribeau@users.noreply.github.com>
2021-04-13 17:33:04 -0400

    Major css edits navbar and menu
    Added placeholder icons for the logo in the menu. Fixed margins for multiple links and divs in the menu.

--- src/containers/NavExpand/NavExpand.js
+++ src/containers/NavExpand/NavExpand.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import PlusButton from "../../assets/Buttons/AddButton.svg"
 
 
 class NavExpand extends Component {
@@ -41,28 +42,30 @@ class NavExpand extends Component {
                 <Link to="#" className="menu-bars">
                     <FaIcons.FaBars onClick={this.showNav}/>
                 </Link>
-                <Navbar.Brand className="navTitle" href="/">Manifest</Navbar.Brand>      
+                <Navbar.Brand className="navTitle" href="/"><h1 className="h1Title">Manifest <img src={PlusButton} alt="Add" style={{width:74, height:74, marginLeft: 0}}/></h1> </Navbar.Brand>      
             </Navbar>
             <nav className={this.state.showSidebar ? "sideNav active" : "sideNav"}>
-            <ul className="navigation">
-                <li className="navToggle">
-                    <Link to="#" className="menu-bars">
-                        <AiIcons.AiOutlineClose onClick={this.showNav}/>
-                    </Link>
-                </li>
-                <li className="navLinks">
-                    <Link to="/">Boards</Link>
-                </li>
-                <li className="navLinks">
-                    <Link to="/About">About</Link>
-                </li>
-                
-               
-           
-                <li className="navLinks">
-                    <Link to="/settings">Settings</Link>
-                </li>
-            </ul>
+                <div className="navigation">
+                    <p className="navToggle">
+                        <Link to="#" className="menu-bars">
+                            <AiIcons.AiOutlineClose onClick={this.showNav}/>
+                        </Link>
+                    </p>
+
+                    <p><center> <img src={PlusButton} alt="Add" style={{width:74, height:74, marginLeft: 0}}/> </center></p>
+
+                    <p className="navLinks">
+                        <Link to="/"> <img src={PlusButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> Boards </Link>
+                    </p>
+
+                    <p className="navLinks">
+                        <Link to="/About"> <img src={PlusButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> About</Link>
+                    </p>
+            
+                    <p className="navLinks">
+                        <Link to="/settings"> <img src={PlusButton} alt="Add" style={{width:34, height:34, marginLeft: 0}}/> Settings</Link>
+                    </p>
+                </div>
             </nav>
         </div>
         )
